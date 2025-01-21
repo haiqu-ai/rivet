8395aed1bedfec9402950f6846e8f959bb7cef60
Mykhailo Ohorodnikov <mohor@softserveinc.com>
2025-01-21 16:06:43 +0200

    optimization - add module

---
+++ rivet_transpiler/optimization.py
@@ -0,0 +1,188 @@
+import qiskit
+
+def remove_unused_qubits(circuit):
+    
+    # Used Qubits
+    
+    used_qubits = []
+    
+    for qubit in circuit.qubits:
+        for instruction in circuit.data:
+
+            if qubit in instruction.qubits:
+                used_qubits.append(qubit)
+                break
+
+    # Registers
+    
+    registers = {register: [] for register in circuit.qregs}
+    
+    for qubit in used_qubits:
+        locations = circuit.find_bit(qubit)
+    
+        for register, index in locations.registers:
+            registers[register].append((qubit, index))
+            
+    # New Registers
+    
+    new_registers = {}
+    qubit_mapping = {}
+    
+    for register, qubits in registers.items():
+    
+        new_register = qiskit.QuantumRegister(
+            size=len(qubits),
+            name=register.name)
+    
+        sorted_qubits = sorted(qubits, key=lambda qubit: qubit[1])
+    
+        for new_index, (qubit, index) in enumerate(sorted_qubits):
+    
+            new_qubit = qiskit.circuit.Qubit(
+                register=new_register,
+                index=new_index)
+    
+            if new_register in new_registers:
+                new_registers[new_register].append(new_qubit)
+            else:
+                new_registers[new_register] = [new_qubit]
+    
+            qubit_mapping[qubit] = new_qubit
+            
+    # New Qubits
+    
+    new_qubits = list(qubit_mapping.values())
+    
+    # New Instructions
+    
+    new_instructions = []
+    
+    for instruction in circuit.data:
+    
+        new_instruction_qubits = [
+            qubit_mapping[qubit]
+            for qubit in instruction.qubits]
+    
+        new_instruction = instruction.replace(
+            operation=instruction.operation,
+            qubits=new_instruction_qubits,
+            clbits=instruction.clbits)
+    
+        new_instructions.append(new_instruction)
+    
+    # New Circuit
+    
+    new_circuit = qiskit.QuantumCircuit()
+    
+    new_circuit.add_register(*new_registers, *circuit.cregs)
+    
+    new_circuit.data = new_instructions
+    
+    new_circuit.global_phase = circuit.global_phase
+    new_circuit.calibrations = circuit.calibrations
+    new_circuit.metadata = circuit.metadata
+    
+    # Layouts
+    
+    layout = circuit.layout
+
+    if layout is not None:
+    
+        initial_layout = layout.initial_layout
+        input_qubit_mapping = layout.input_qubit_mapping
+        final_layout = layout.final_layout
+        
+        output_qubit_list = layout._output_qubit_list
+        
+        # Used Indices
+        
+        used_indices = [index for index, qubit 
+                        in enumerate(output_qubit_list)
+                        if qubit in used_qubits]
+        
+        # Used Input Qubits
+        
+        sorted_initial_layout = dict(sorted(
+            initial_layout.get_physical_bits().items()))
+
+        used_input_qubits = [qubit for index, qubit 
+                             in sorted_initial_layout.items()
+                             if index in used_indices]
+        
+        # New Input Qubit Mapping
+        
+        shift = 0
+        new_input_qubit_mapping = {}
+        
+        for qubit, index in input_qubit_mapping.items():
+        
+            if qubit in used_input_qubits:
+                new_input_qubit_mapping[qubit] = index - shift
+            else:
+                shift += 1
+                
+        # Index Mapping
+        
+        index_mapping = {}
+        
+        for new_index, qubit in enumerate(used_input_qubits):
+        
+            index = initial_layout[qubit]
+            index_mapping[index] = new_index
+        
+        # New Initial Layout
+        
+        new_initial_layout_dict = {}
+        
+        for index, qubit in initial_layout.get_physical_bits().items():
+        
+            if qubit not in used_input_qubits:
+                continue
+        
+            new_index = index_mapping[index]
+            new_initial_layout_dict[new_index] = qubit
+        
+        new_initial_layout = layout.initial_layout.copy()
+        
+        new_initial_layout._v2p.clear()
+        new_initial_layout._p2v.clear()
+        
+        for index, qubit in new_initial_layout_dict.items():
+        
+            new_initial_layout._v2p[qubit] = index
+            new_initial_layout._p2v[index] = qubit
+        
+        # New Final Layout
+        
+        if final_layout is None:
+            new_final_layout = None
+
+        else:
+            
+            new_final_layout_dict = {}
+            
+            for index, qubit in final_layout.get_physical_bits().items():
+        
+                if qubit not in used_qubits:
+                    continue
+            
+                new_qubit = qubit_mapping[qubit]
+                new_index = index_mapping[index]
+                
+                new_final_layout_dict[new_index] = new_qubit
+            
+            new_final_layout = qiskit.transpiler.Layout(
+                new_final_layout_dict)
+        
+        # New Layout
+        
+        new_layout = qiskit.transpiler.TranspileLayout(
+            initial_layout=new_initial_layout,
+            input_qubit_mapping=new_input_qubit_mapping,
+            final_layout=new_final_layout,
+            _input_qubit_count = len(new_qubits),
+            _output_qubit_list = new_qubits)
+        
+        new_circuit._layout = new_layout
+    
+    return new_circuit
